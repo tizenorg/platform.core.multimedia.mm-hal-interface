18d85795c887edd3baa61314d8e126bea70fffce
Gilbok Lee <gilbok.lee@samsung.com>
2016-06-10 16:06:06 +0900

    [Radio] Initial release for radio HAL
    Change-Id: I06f3bfcbeee7f67fb1b673eab9992db65a44288e
    Signed-off-by: Gilbok Lee <gilbok.lee@samsung.com>

--- radio/tizen-radio.h
+++ radio/tizen-radio.h
@@ -17,3 +17,75 @@
  *
  */
 
+#ifndef __TIZEN_RADIO_HAL_H__
+#define __TIZEN_RADIO_HAL_H__
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+typedef enum radio_return {
+	RADIO_ERROR_NONE,
+	RADIO_ERROR_INVALID_PARAMETER,
+	RADIO_ERROR_PERMITSSION_DENIED,
+	RADIO_ERROR_DEVICE_OPEN,
+	RADIO_ERROR_DEVICE_NOT_SUPPORTED,
+	RADIO_ERROR_INTERNAL,
+	RADIO_ERROR_UNKNOWN
+} radio_return_t;
+
+typedef enum radio_seek_direction_type {
+	RADIO_SEEK_UP,					/**< Seek upward */
+	RADIO_SEEK_DOWN,				/**< Seek downward */
+	RADIO_SEEK_NUM,				/**< Number of seeking direction */
+} radio_seek_direction_type_t;
+
+typedef struct radio_interface {
+	/* create & destroy */
+	radio_return_t (*init)(void **radio_hanle);
+	radio_return_t (*deinit)(void *radio_handle);
+	radio_return_t (*open_device)(void *radio_handle);
+	radio_return_t (*close_device)(void *radio_handle);
+	radio_return_t (*prepare_device)(void *radio_handle);
+	radio_return_t (*unprepare_device)(void *radio_handle);
+	radio_return_t (*get_state)(void *radio_handle, uint32_t *state);
+	radio_return_t (*seek)(void *radio_handle, radio_seek_direction_type_t direction);
+	radio_return_t (*get_frequency)(void *radio_handle, uint32_t *frequency);
+	radio_return_t (*set_frequency)(void *radio_handle, uint32_t frequency);
+	radio_return_t (*scan_start)(void *radio_handle);
+	radio_return_t (*scan_stop)(void *radio_handle);
+	radio_return_t (*mute)(void *radio_handle);
+	radio_return_t (*unmute)(void *radio_handle);
+	radio_return_t (*get_volume)(void *radio_handle, uint32_t *volume);
+	radio_return_t (*set_volume)(void *radio_handle, uint32_t volume);
+	radio_return_t (*get_signal_strength)(void *radio_handle, uint32_t *strength);
+	radio_return_t (*set_band_range)(void *radio_handle, uint32_t rangelow, uint32_t rangehigh);
+} radio_interface_t;
+
+radio_return_t radio_init(void **radio_hanle);
+radio_return_t radio_deinit(void *radio_handle);
+radio_return_t radio_open_device(void *radio_handle);
+radio_return_t radio_close_device(void *radio_handle);
+radio_return_t radio_prepare_device(void *radio_handle);
+radio_return_t radio_unprepare_device(void *radio_handle);
+radio_return_t radio_get_state(void *radio_handle, uint32_t *state);
+radio_return_t radio_seek(void *radio_handle, radio_seek_direction_type_t direction);
+radio_return_t radio_get_frequency(void *radio_handle, uint32_t *frequency);
+radio_return_t radio_set_frequency(void *radio_handle, uint32_t frequency);
+radio_return_t radio_scan_start(void *radio_handle);
+radio_return_t radio_scan_stop(void *radio_handle);
+radio_return_t radio_mute(void *radio_handle);
+radio_return_t radio_unmute(void *radio_handle);
+radio_return_t radio_get_volume(void *radio_handle, uint32_t *volume);
+radio_return_t radio_set_volume(void *radio_handle, uint32_t volume);
+radio_return_t radio_get_signal_strength(void *radio_handle, uint32_t *strength);
+radio_return_t radio_set_band_range(void *radio_handle, uint32_t rangelow, uint32_t rangehigh);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* __TIZEN_RADIO_HAL_H__ */
+
